93d17e79dd661eb2886896b619fbd60cf4e8682b
Sergey Timchenko <art-street93@mail.ru>
2016-10-18 12:56:26 +0300

    Initial commit

---
+++ js/docs-setup.js
@@ -0,0 +1,318 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "Meals.CartFactory",
+      "shortName": "Meals.CartFactory",
+      "type": "service",
+      "moduleName": "Meals",
+      "shortDescription": "Factory work with cart.",
+      "keywords": "add addmealtocartlist addmodifierstolist amount api array cart cartfactory count deletemodifiers edit elements factory fo getcartlist getcartlistcount getindexeditmeal getmodifiersprice gettotalprice length list meal meals method modif modifiers modifiersarr object objects price remove removemeal reset return service set setindexeditmeal total work"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:AppController",
+      "shortName": "AppController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;appPage&quot;.",
+      "keywords": "api appcontroller apppage component controller meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:CartController",
+      "shortName": "CartController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;cartPage&quot;.",
+      "keywords": "ability api array cartcontroller cartfactory cartlist cartpage choice class component controller currency current display drinks edit editmeal meal meals meals-controller-cartcontroller-page meals-controller-page mealsfactory method object open price select total totalprice user-selected"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:CartIconController",
+      "shortName": "CartIconController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;cartIconPage&quot;.",
+      "keywords": "api carticoncontroller carticonpage component controller meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:DrinksListController",
+      "shortName": "DrinksListController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;drinksList&quot;.",
+      "keywords": "activate api array assigning call choice class component controller currency data drink drinks drinkslist drinkslistcontroller drinksobj getdrinks getdrinksforview handle indicating initialize meals meals-controller-drinkslistcontroller-page meals-controller-page mealsfactory method object onlysoftdrinks opendrink openmeal owned properties return select soft user variable"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:ErrorController",
+      "shortName": "ErrorController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;errorPage&quot;.",
+      "keywords": "api component controller errorcontroller errorpage meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:FooterController",
+      "shortName": "FooterController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;footerPage&quot;.",
+      "keywords": "api component controller footercontroller footerpage meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:HeaderController",
+      "shortName": "HeaderController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;headerPage&quot;.",
+      "keywords": "api component controller headercontroller headerpage meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:LogoController",
+      "shortName": "LogoController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;logoPage&quot;.",
+      "keywords": "api component controller logocontroller logopage meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:MainController",
+      "shortName": "MainController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;mainPage&quot;.",
+      "keywords": "api component controller maincontroller mainpage meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:MealController",
+      "shortName": "MealController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;mealPage&quot;.",
+      "keywords": "add addmeal addmealtocartlist allows amount api call cart cartcount cartfactory class component controller count currency current currentmeal delete deletemodifiers edit meal mealcontroller mealpage meals meals-controller-mealcontroller-page meals-controller-page mealsfactory mealstatus method owned removemeal savemeal select selectamount status user"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:MealsListController",
+      "shortName": "MealsListController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;mealsList&quot;.",
+      "keywords": "activate api array assigning call choice class component controller currency data getproducts handle initialize meal meals meals-controller-mealslistcontroller-page meals-controller-page mealsfactory mealslist mealslistcontroller mealsobj method object openmeal owned products properties user"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:MenuController",
+      "shortName": "MenuController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;menuPage&quot;.",
+      "keywords": "api component controller meals menucontroller menupage"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:ModifiersController",
+      "shortName": "ModifiersController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;modifiersPage&quot;.",
+      "keywords": "api array call checkbox choice class clean cleanpickmodifiers component controller handle meals meals-controller-modifierscontroller-page meals-controller-page mealsfactory method modifier modifiers modifierscontroller modifiersfactory modifierspage object owned pick selectmodifier user"
+    },
+    {
+      "section": "api",
+      "id": "Meals.controller:SelectCountController",
+      "shortName": "SelectCountController",
+      "type": "controller",
+      "moduleName": "Meals",
+      "shortDescription": "Controller component &quot;selectCount&quot;.",
+      "keywords": "active activenum amount api button captures choice class component controller false function meal meals method num return select selectcount selectcountcontroller selectnum true user variable"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:appPage",
+      "shortName": "appPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface of app.",
+      "keywords": "api app apppage component directive interface meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:cartIconPage",
+      "shortName": "cartIconPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface of cart button and string with meals count.",
+      "keywords": "accepts api button cart carticonpage component controller count directive interface meals parent string variable"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:cartPage",
+      "shortName": "cartPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface to cart page.",
+      "keywords": "api cart cartpage component directive interface meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:drinksList",
+      "shortName": "drinksList",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface to list of drinks.",
+      "keywords": "api component directive drinks drinkslist interface list meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:errorPage",
+      "shortName": "errorPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface of error page.",
+      "keywords": "api component directive error errorpage interface meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:footerPage",
+      "shortName": "footerPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface to footer section.",
+      "keywords": "api component directive footer footerpage interface meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:headerPage",
+      "shortName": "headerPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface to header section.",
+      "keywords": "api component directive header headerpage interface meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:logoPage",
+      "shortName": "logoPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface to logo section.",
+      "keywords": "api component directive interface logo logopage meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:mainPage",
+      "shortName": "mainPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface to main section.",
+      "keywords": "api component directive interface main mainpage meals"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:mealPage",
+      "shortName": "mealPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface of meal.",
+      "keywords": "accepts api component controller directive interface meal mealpage meals parent path user variable"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:mealsList",
+      "shortName": "mealsList",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface to list of meals.",
+      "keywords": "api component directive interface list meals mealslist"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:menuPage",
+      "shortName": "menuPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface to menu section.",
+      "keywords": "api component directive interface meals menu menupage"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:modifiersPage",
+      "shortName": "modifiersPage",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface of modifiers section.",
+      "keywords": "accepts api component controller currency directive interface meals modifiers modifierspage parent variable"
+    },
+    {
+      "section": "api",
+      "id": "Meals.directive:selectCount",
+      "shortName": "selectCount",
+      "type": "directive",
+      "moduleName": "Meals",
+      "shortDescription": "Component provides an interface to select amount of meal.",
+      "keywords": "amount api component controller directive interface meal meals parent select selectcount selected transfers variable"
+    },
+    {
+      "section": "api",
+      "id": "Meals.MealsFactory",
+      "shortName": "Meals.MealsFactory",
+      "type": "service",
+      "moduleName": "Meals",
+      "shortDescription": "Factory get currency, array of products and array of drinks,",
+      "keywords": "$http $q amount api array currency current data deferred drinks edit factory false foods getcurrency getcurrentmeal getcurrentmealamount getcurrentmealstatus getmeals getmealsfromhttp getmodifiers meal meals mealsfactory method modifiers object obtaining openmeal products promise received reject request resolve response return server service set setcurrentmeal setcurrentmealamount setcurrentmealbyid setcurrentmealstatus status work"
+    },
+    {
+      "section": "api",
+      "id": "Meals.ModifiersFactory",
+      "shortName": "Meals.ModifiersFactory",
+      "type": "service",
+      "moduleName": "Meals",
+      "shortDescription": "Factory work with modifiers.",
+      "keywords": "add api array cartfactory checked checkedmodifiers current delete deleted deletemodifier factory false getcheckedmodifiers meal meals method modifier modifierhasbeenselected modifiers modifiersfactory object objects pick pickcheckedmodifiers return selected selectedmodifiers selectmodifier service true work"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js"
+  ]
+};
